0e1412ba914bd669b1c8e2ce83de66fdaf704cc6
dahyeKim92 <dasangtogether@gmail.com>
2024-11-05 17:54:35 +0900

    Update MemberMapper.java

--- src/main/java/org/zerock/member/mapper/MemberMapper.java
+++ src/main/java/org/zerock/member/mapper/MemberMapper.java
@@ -1,11 +1,16 @@
 package org.zerock.member.mapper;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import org.zerock.member.vo.LoginVO;
+import org.zerock.util.page.PageObject;
 
 @Repository
 public interface MemberMapper {
 
 	// login
 	public LoginVO login(LoginVO vo);
+
+	public List<LoginVO> list(PageObject pageObject);
 }

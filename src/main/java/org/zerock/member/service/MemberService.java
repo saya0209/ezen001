b0f0c0131623d5b2515e5d7c802643814091e761
dahyeKim92 <dasangtogether@gmail.com>
2024-11-05 17:54:57 +0900

    Update MemberService.java

--- src/main/java/org/zerock/member/service/MemberService.java
+++ src/main/java/org/zerock/member/service/MemberService.java
@@ -1,9 +1,15 @@
 package org.zerock.member.service;
 
+import java.util.List;
+
 import org.zerock.member.vo.LoginVO;
+import org.zerock.util.page.PageObject;
 
 public interface MemberService {
 
 	// 로그인
 	public LoginVO login(LoginVO vo);
+	
+	// 리스트
+	public List<LoginVO> list(PageObject pageObject);
 }

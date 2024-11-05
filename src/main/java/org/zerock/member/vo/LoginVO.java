d8f7c9ca2473908d2b9b2bd63906fc69fe1ff93a
dahyeKim92 <dasangtogether@gmail.com>
2024-11-05 17:55:40 +0900

    Update LoginVO.java

--- src/main/java/org/zerock/member/vo/LoginVO.java
+++ src/main/java/org/zerock/member/vo/LoginVO.java
@@ -2,12 +2,9 @@ package org.zerock.member.vo;
 
 import java.util.Date;
 
-import lombok.Data;
-
-@Data
 public class LoginVO {
 
-	   private Integer status;
+	   private String status;
 	   private String id;
 	   private String pw;
 	   private String nicname;
@@ -21,6 +18,79 @@ public class LoginVO {
 	   private String gradeName;
 	   private String grade_image;
 	   
-	
+	//getter and setter 
+	   
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNicname() {
+		return nicname;
+	}
+	public void setNicname(String nicname) {
+		this.nicname = nicname;
+	}
+	public Integer getTel() {
+		return tel;
+	}
+	public void setTel(Integer tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getConDate() {
+		return conDate;
+	}
+	public void setConDate(Date conDate) {
+		this.conDate = conDate;
+	}
+	public Integer getGradeNo() {
+		return gradeNo;
+	}
+	public void setGradeNo(Integer gradeNo) {
+		this.gradeNo = gradeNo;
+	}
+	public String getGradeName() {
+		return gradeName;
+	}
+	public void setGradeName(String gradeName) {
+		this.gradeName = gradeName;
+	}
+	public String getGrade_image() {
+		return grade_image;
+	}
+	public void setGrade_image(String grade_image) {
+		this.grade_image = grade_image;
+	}
 	
 }

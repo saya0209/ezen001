fd0bfa7bc4ec03cebe079bca43f79895d430f55c
book113 <mrbook1113t@gmail.com>
2024-11-05 12:12:03 +0900

    Update NoticeVO.java

--- src/main/java/org/zerock/notice/vo/NoticeVO.java
+++ src/main/java/org/zerock/notice/vo/NoticeVO.java
@@ -5,19 +5,22 @@ import java.util.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
+import oracle.net.aso.p;
 
 @Data
 public class NoticeVO {
 	
-	private Long no;
+	private Long notice_no;
+	private String writer_id;
 	private String title;
 	private String content;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date writeDate;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date startDate;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date endDate;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date writeDate;
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date updateDate;
+	private String image;
 }
